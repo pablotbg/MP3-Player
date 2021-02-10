165c5989073fd098d293c093cce54f74dd15de9b
Paweł Praga <p.praga@o2.pl>
2021-02-10 14:56:56 +0100

    First commit with Main class

---
+++ src/main/java/mp3player/main/Main.java
@@ -0,0 +1,17 @@
+package mp3player.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Mp3 Player");
+        primaryStage.show();
+    }
+}
